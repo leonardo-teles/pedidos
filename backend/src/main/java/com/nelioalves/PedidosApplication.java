e0e20f7be90a3eed46a3e0c03e68e82b697dd0d7
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-05-04 19:39:06 -0300

    Cliente, TipoCliente, Telefones e Endereços

--- backend/src/main/java/com/nelioalves/PedidosApplication.java
+++ backend/src/main/java/com/nelioalves/PedidosApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.domain.Categoria;
 import com.nelioalves.domain.Cidade;
+import com.nelioalves.domain.Cliente;
+import com.nelioalves.domain.Endereco;
 import com.nelioalves.domain.Estado;
 import com.nelioalves.domain.Produto;
+import com.nelioalves.enums.TipoCliente;
 import com.nelioalves.repositories.CategoriaRepository;
 import com.nelioalves.repositories.CidadeRepository;
+import com.nelioalves.repositories.ClienteRepository;
+import com.nelioalves.repositories.EnderecoRepository;
 import com.nelioalves.repositories.EstadoRepository;
 import com.nelioalves.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class PedidosApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
@@ -52,7 +63,6 @@ public class PedidosApplication implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat1));	
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Rio de Janeiro");
@@ -67,5 +77,16 @@ public class PedidosApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Leonardo Almeida", "leonardo@gmail.com", "03746549728", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("991054374", "979615072"));
+		
+		Endereco e1 = new Endereco(null, "Estrada da Cachamorra", "1233", "bloco 11 apto 210", "Campo Grande", "23040150", cli1, c1);
+		Endereco e2 = new Endereco(null, "Rua Murilo Alvarenga", "65", "bloco 2 apto 206", "Campo Grande", "22090180", cli1, c1);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 }
