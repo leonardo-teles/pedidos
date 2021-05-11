f6fe67d55d1d7fc52ae50bab5907588713d314ae
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-05-11 16:15:14 -0300

    Inserindo Pedido

--- backend/src/main/java/com/nelioalves/resources/PedidoResource.java
+++ backend/src/main/java/com/nelioalves/resources/PedidoResource.java
@@ -1,11 +1,18 @@
 package com.nelioalves.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.domain.Pedido;
 import com.nelioalves.services.PedidoService;
@@ -22,5 +29,15 @@ public class PedidoResource {
 		Pedido obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> inserir(@Valid @RequestBody Pedido obj) {
+
+		obj = service.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
--- backend/src/main/java/com/nelioalves/services/PedidoService.java
+++ backend/src/main/java/com/nelioalves/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.nelioalves.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nelioalves.domain.ItemPedido;
+import com.nelioalves.domain.PagamentoComBoleto;
 import com.nelioalves.domain.Pedido;
+import com.nelioalves.enums.EstadoPagamento;
+import com.nelioalves.repositories.ItemPedidoRepository;
+import com.nelioalves.repositories.PagamentoRepository;
 import com.nelioalves.repositories.PedidoRepository;
 import com.nelioalves.services.exception.ObjectNotFoundException;
 
@@ -15,9 +22,48 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> opt = repo.findById(id);
 		
 		return opt.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENDTE);
+		obj.getPagamento().setPedido(obj);
+
+		//gera a data de vencimento(7 dias após a data atual) com uma classe auxiliar, caso o pagamento seja com boleto
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pgto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pgto, obj.getInstante());
+		}
+
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+
+		itemPedidoRepository.saveAll(obj.getItens());
+
+		return obj;
 	}
 }
