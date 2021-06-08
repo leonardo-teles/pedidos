37d87d3a1e8e8a4cd45d7551c6200806e7106998
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-06-08 10:34:42 -0300

    Implementando Autorização

--- backend/src/main/java/com/nelioalves/config/SecurityConfig.java
+++ backend/src/main/java/com/nelioalves/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.nelioalves.security.JWTAuthenticationFilter;
+import com.nelioalves.security.JWTAuthorizationFilter;
 import com.nelioalves.security.JWTUtil;
 
 @Configuration
@@ -61,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 			
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
